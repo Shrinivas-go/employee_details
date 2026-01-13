786cea1816b09192a849b58a63eb7be854e77d39
Shrinivas-go <shrinivas4927@gmail.com>
2026-01-13 15:10:18 +0530

    saving

--- employee.py
+++ employee.py
@@ -1,9 +1,9 @@
 def employee_details(name, age):
-  result = (
-    f"Name: {name}\n"
-    f" Age: {age}\n"
-  )
-  return result
+    result = (
+        f"Name: {name}\n"
+        f"Age: {age}\n" 
+    )
+    return result
 if __name__ == "__main__":
   name = "Shrinivas"
   age = 20
--- test_employee.py
+++ test_employee.py
@@ -1,7 +1,5 @@
 from employee import employee_details
+
 def test_employee_details():
-  expected = (
-    "Name: Shrinivas\n"
-    "Age: 20\n"
-  )
-  assert employee_details("Shrinivas" , 20) == expected
+    expected = "Name: Shrinivas\nAge: 20\n"
+    assert employee_details("Shrinivas", 20) == expected
